8195a419f1d4a989f9a829dfebb2597240ac438f
sakethreddy-24 <sakethreddy1129@gmail.com>
2026-09-12 18:57:51 +0000

    feat(backend): add category domain model

--- backend/app/database/models/__init__.py
+++ backend/app/database/models/__init__.py
@@ -1,11 +1,13 @@
 from app.database.models.profile import UserProfile
 from app.database.models.language import Language
 from app.database.models.region import Region
+from app.database.models.category import Category
 from app.database.models.storyteller import Storyteller
 
 __all__ = [
     "UserProfile",
     "Language",
     "Region",
+    "Category",
     "Storyteller",
 ]
